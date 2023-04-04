35b5664475469856d9884a51af92a083a5e1258d
Sofya <s.v.chistova01@gmail.com>
2023-04-04 18:19:25 +0300

    Тестирование работы модели-представления главного окна

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CV19.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace CV19.ViewModels
 {
-    internal class MainWindowViewModel
+    #region Заголовок окна
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "Анализ статистики CV19";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
+
+    #endregion
 }
